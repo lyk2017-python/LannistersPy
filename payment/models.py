42dec0cac5383b2fa82d46034f9ec75376d006f2
Duygu Gündoğdu <duygugundogdu7@hotmail.com>
2017-07-25 19:03:24 +0300

    models.py updated with uuid generators

--- payment/models.py
+++ payment/models.py
@@ -12,16 +12,21 @@ class Product(models.Model):
     count = models.PositiveIntegerField()
 
 
+def uuidGeneratorPrepaid(self):
+    code = str(uuid.uuid4())
+    return code[:10]
+
 class PrepaidCard(models.Model):
-    barcode = models.CharField(max_length=8)
+    barcode = models.CharField(max_length=8,default=uuidGeneratorPrepaid)
     value = models.PositiveIntegerField()
+
+def uuidGeneratorUserCard(self):
+    code = str(uuid.uuid4())
+    return code[:18]
 
 
 class UserCard(models.Model):
-    card_number = models.CharField(max_length=16)
+    card_number = models.CharField(max_length=16, default=uuidGeneratorUserCard)
     balance = models.PositiveIntegerField()
 
 
-    def uuidGenerator(self):
-        code = str(uuid.uuid4())
-        return code[:18]
