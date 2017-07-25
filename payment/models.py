7de6e48b8b7cabb2a729b99ed01fd03354159a4a
Duygu Gündoğdu <duygugundogdu7@hotmail.com>
2017-07-25 17:13:04 +0300

    models.py updated

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    price = models.FloatField()
+    brand = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField()
+    url = models.URLField()
+    count = models.PositiveIntegerField()
+
+
+class PrepaidCard(models.Model):
+    number = models.CharField(max_length=8)
+    value = models.PositiveIntegerField()
+
+
+class UserCard(models.Model):
+    card_number = models.CharField(max_length=16)
+    balance = models.PositiveIntegerField()
+
