235f3412963774216c8698b4dd97dcea7d3a4863
Ahmed Ihsan Erdem <ahmedihsanerdem@gmail.com>
2017-07-26 11:48:00 +0300

    views updated.

--- payment/urls.py
+++ payment/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from payment import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView, name="index"),
-    url(r'^vendor/(?P<vendor_id>\d+)/$', views.VendorView, name="vendor"),
-    url(r'^product/(?P<product_slug>\w+)\$', views.ProductView, name="product"),
+    url(r'^$', views.IndexView.as_view(), name="index"),
+    url(r'^vendor/(?P<vendor_id>\d+)/$', views.VendorView.as_view(), name="vendor"),
+    url(r'^product/(?P<product_slug>\w+)\$', views.ProductView.as_view(), name="product"),
 ]
