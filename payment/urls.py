4c95404078b34ca65692074b6d90b012aa971f7f
dorukgezici <dorukgezici@gmail.com>
2017-07-26 11:29:48 +0300

    Created views.

--- payment/urls.py
+++ payment/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from payment import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
-    url(r'^vendor/(?P<vendor_id>\d+)/$')
+    url(r'^$', views.IndexView, name="index"),
+    url(r'^vendor/(?P<vendor_id>\d+)/$', views.VendorView, name="vendor"),
+    url(r'^product/(?P<product_slug>\w+)\$', views.ProductView, name="product"),
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.views import generic
+from payment.models import Vendor, Product
 
 # Create your views here.
 
 class IndexView(generic.ListView):
     def get_queryset(self):
         return Vendor.objects.all()
+
+
+class VendorView(generic.ListView):
+    def get_queryset(self):
+        return Product.objects.all()
+
+
+class ProductView(generic.DetailView):
+    model = Product
