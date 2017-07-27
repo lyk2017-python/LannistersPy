e61187270a7da86b7b54869759c4a91c2db3cf7a
dorukgezici <dorukgezici@gmail.com>
2017-07-27 18:19:14 +0300

    Admin classes fixed.

--- payment/admin.py
+++ payment/admin.py
@@ -1,68 +1,63 @@
 from django.contrib import admin
-from payment.models import Product, UserCard, PrepaidCard, Vendor
+from payment.models import Product, UserCard, PrepaidCard, Vendor, Inventory
 
-# Product
-# UserCard
-# PrepaidCard
-# Vendor
 
-# class VendorInline (admin.StackedInline):
-#     model = Vendor.product.through
-#     extra = 0
-#
-# @admin.register(Product)
-# class ProductAdmin (admin.ModelAdmin):
-#     list_display = [
-#         "name",
-#         "brand",
-#         "price",
-#         "count",
-#     ]
-#     search_fields = [
-#         "name",
-#         "brand",
-#         "price",
-#     ]
-#     list_filter = [
-#         "name", # Filters in list screen.
-#         "brand",
-#         "price",
-#     ]
-#
-#
-# @admin.register(UserCard)
-# class UserCardAdmin (admin.ModelAdmin):
-#     list_display = [
-#         "card_number",
-#         "balance",
-#     ]
-#     search_fields = [
-#         "card_number",
-#         "balance",
-#     ]
-#
-#
-# @admin.register(PrepaidCard)
-# class PrepaidCardAdmin (admin.ModelAdmin):
-#     list_display = [
-#         "barcode",
-#         "value",
-#     ]
-#     search_fields = [
-#         "barcode",
-#         "value",
-#     ]
-#
-#
-# @admin.register(Vendor)
-# class VendorAdmin (admin.ModelAdmin):
-#     inlines = [VendorInLine]
-#     list_display = [
-#         "location",
-#     ]
-#     search_fields = [
-#         "location",
-#     ]
-#     list_filter = [
-#         "location"  # Filters in list screen.
-#     ]
+class VendorInline(admin.StackedInline):
+    model = Inventory
+    extra = 0
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "brand",
+        "price",
+    ]
+    search_fields = [
+        "name",
+        "brand",
+        "price",
+    ]
+    list_filter = [
+        "name", # Filters in list screen.
+        "brand",
+        "price",
+    ]
+
+
+@admin.register(UserCard)
+class UserCardAdmin(admin.ModelAdmin):
+    list_display = [
+        "card_number",
+        "balance",
+    ]
+    search_fields = [
+        "card_number",
+        "balance",
+    ]
+
+
+@admin.register(PrepaidCard)
+class PrepaidCardAdmin(admin.ModelAdmin):
+    list_display = [
+        "barcode",
+        "value",
+    ]
+    search_fields = [
+        "barcode",
+        "value",
+    ]
+
+
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    inlines = [VendorInline]
+    list_display = [
+        "location",
+    ]
+    search_fields = [
+        "location",
+    ]
+    list_filter = [
+        "location"  # Filters in list screen.
+    ]
