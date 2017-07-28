198f877935135b257f07d0920718359ab850863f
dorukgezici <dorukgezici@gmail.com>
2017-07-28 19:52:27 +0300

    New database.

--- LannistersPy/settings.py
+++ LannistersPy/settings.py
@@ -100,6 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "payment.ExtendedUser"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 import uuid
 import datetime
@@ -16,35 +17,33 @@ class Product(models.Model):
     slug = models.SlugField(unique=True, blank=True, null=False)
 
     def __str__(self):
-
         return "{name}".format(name=self.name)
-    # Model options
 
     class Meta:
         get_latest_by = "name"
         unique_together = ("name", "brand")
 
 
-def uuidGenerator8():
+def uuid_generator8():
     code = uuid.uuid4().hex
     return code[:8]
 
 
 class PrepaidCard(models.Model):
-    barcode = models.CharField(max_length=8, default=uuidGenerator8, unique=True)
+    barcode = models.CharField(max_length=8, default=uuid_generator8, unique=True)
     value = models.PositiveIntegerField()
 
     def __str__(self):
         return "{barcode}".format(barcode=self.barcode)
 
 
-def uuidGenerator16():
+def uuid_generator16():
     code = uuid.uuid4().hex
     return code[:16]
 
 
 class UserCard(models.Model):
-    card_number = models.CharField(max_length=16, default=uuidGenerator16, unique=True)
+    card_number = models.CharField(max_length=16, default=uuid_generator16, unique=True)
     balance = models.PositiveIntegerField(default=0)
 
     def __str__(self):
@@ -67,14 +66,16 @@ class Inventory(models.Model):
         return "{vendor} -> {product} #{count}".format(vendor=self.vendor, product=self.product, count=self.count)
 
 
-class Comment (models.Model):
+class Comment(models.Model):
     created = models.DateTimeField(default=datetime.datetime.now)
     message = models.TextField()
     product = models.ForeignKey(Product)
+
     # author field will be added
 
     def __str__(self):
         return "{message} ".format(message=self.message)
+
 
 @receiver(pre_save, sender=Product)
 def generate_slug(sender, instance, *args, **kwargs):
@@ -84,3 +85,7 @@ def generate_slug(sender, instance, *args, **kwargs):
         else:
             raise AttributeError("Name field is required for slug.")
     return instance
+
+
+class ExtendedUser(AbstractUser):
+    card = models.OneToOneField(UserCard, null=True)
